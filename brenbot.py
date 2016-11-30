a5a42812440f8102412a28bebf3e0409d8b83a9f
Andrew Thomas <andrew@cjthomas.org>
2016-11-30 17:15:58 -0600

    Added logging

--- brenbot.py
+++ brenbot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+import logging
+import logging.handlers
 import os
 import sys
 import time
@@ -29,6 +30,41 @@ REACTIONS = []
 # Create the running var
 IS_RUNNING = True
 
+# Deafults for logging
+LOG_FILENAME = "/tmp/Brenbot.log"
+LOG_LEVEL = logging.INFO  # Could be e.g. "DEBUG" or "WARNING"
+
+# Configure logging to log to a file, making a new file at midnight and keeping the last 3 day's data
+# Give the logger a unique name (good practice)
+logger = logging.getLogger(__name__)
+# Set the log level to LOG_LEVEL
+logger.setLevel(LOG_LEVEL)
+# Make a handler that writes to a file, making a new file at midnight and keeping 3 backups
+handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when="midnight", backupCount=3)
+# Format each log message like this
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+# Attach the formatter to the handler
+handler.setFormatter(formatter)
+# Attach the handler to the logger
+logger.addHandler(handler)
+
+# Make a class we can use to capture stdout and sterr in the log
+class MyLogger(object):
+        def __init__(self, logger, level):
+                """Needs a logger and a logger level."""
+                self.logger = logger
+                self.level = level
+
+        def write(self, message):
+                # Only log if there is a message (not just a new line)
+                if message.rstrip() != "":
+                        self.logger.log(self.level, message.rstrip())
+
+# Replace stdout with logging to file at INFO level
+sys.stdout = MyLogger(logger, logging.INFO)
+# Replace stderr with logging to file at ERROR level
+sys.stderr = MyLogger(logger, logging.ERROR)
+
 
 def main():
     global REACTIONS, REACT_TO_USERS, IS_RUNNING, BOT_ID, AT_BOT, __location__
@@ -53,8 +89,10 @@ def main():
             motd.join()
         else:
             print("Connection failed. Invalid Slack token?")
-    except:
+    except Exception as exception:
         IS_RUNNING = False
+        print(str(exception))
+
 
 
 def reactions_loop():
